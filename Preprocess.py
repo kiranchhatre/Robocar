8015885f1bcabbcfa86b5d5a92820bf51d6ff3e8
Julius Faubel <faubel.julius@gmail.com>
2019-05-20 15:42:17 +0200

    Commit function

---
+++ Preprocess.py
@@ -0,0 +1,33 @@
+from cv2 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+
+img_maze = cv2.imread('C:\\Users\\faube\\Desktop\\Python\\Data\\maze_hard.jpg')
+rgb_image = cv2.cvtColor(img_maze, cv2.COLOR_BGR2RGB)
+
+#Gray conversion
+gray_image = cv2.cvtColor(img_maze, cv2.COLOR_RGB2GRAY)
+
+#Blur
+gray_image= cv2.GaussianBlur(gray_image,(21,21),0)
+
+#Threshold
+ret, threshold_image = cv2.threshold(gray_image,110, 255, 0)
+#Resize
+resized_image= cv2.resize(threshold_image, (0,0), fx=0.2, fy=0.2)
+#Invert 
+image_final = resized_image
+#image_final = cv2.bitwise_not(resized_image)
+
+#Display
+cv2.startWindowThread()
+#cv2.namedWindow("Image")
+cv2.imshow('Image',image_final)
+cv2.waitKey(5)
+
+#SaveFile
+
+cv2.imwrite('C:\\Users\\faube\\Desktop\\Python\\Data\\Maze_hardfinal.jpg',image_final)
+
---
+++ Robot.py
@@ -0,0 +1,7 @@
+from cv2 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+#Import as Array
+route = np.loadtxt('C:\\Users\\faube\\Desktop\\Python\\Route.txt',dtype=int, delimiter=',')
